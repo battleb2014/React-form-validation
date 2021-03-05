3c14fdd1fed1690a92145d3c87fc1e06783653a5
Brandon <battleb2014@gmail.com>
2021-03-05 13:46:15 -0500

    Main react files installed

---
+++ form-validation/src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div id="header-container" class="header-container">
+            <span>Register for</span>
+            <h1>Full Stack Conference</h1>
+        </div>
+    );
+}
+
+export default Header;
